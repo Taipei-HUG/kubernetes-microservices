fdebe867e601ad7b7d5a9e14e9d29748da40fc44
Alan Chen <cchck91@gmail.com>
2020-03-19 21:29:14 +0800

    add download-binary.sh

---
+++ setup/download-bianry.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+mkdir ~/bin
+
+HOST_OS=$(uname -s)
+
+echo "Get binaries ..."
+
+curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+echo 'helm Done.'
+
+curl --silent -Lo kubectl curl -LO https://storage.googleapis.com/kubernetes-release/release/1.17.4/bin/$HOST_OS/amd64/kubectl && chmod +x kubectl && mv kubectl ~/bin/
+echo 'kubectl Done.'
+
+curl --silent -Lo aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/$HOST_OS/amd64/aws-iam-authenticator && chmod +x aws-iam-authenticator
+sudo mv aws-iam-authenticator /usr/bin
+echo 'aws-iam-authenticator Done.'
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+echo 'eksctl Done.'
+
+curl --silent --location "https://github.com/txn2/kubefwd/releases/download/v1.9.3/kubefwd_linux_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/kubefwd /usr/bin
+echo 'kubefwd Done.'
+
+curl --silent -Lo kubebox https://github.com/astefanutti/kubebox/releases/download/v0.6.1/kubebox-linux && chmod +x kubebox
+sudo mv kubebox /usr/local/bin
+echo 'kubebox Done.'
+
+curl --silent -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
+chmod +x skaffold
+sudo mv skaffold /usr/local/bin
+echo 'skaffold Done.'
+
+curl -o terraform_0.12.13_linux_amd64.zip https://releases.hashicorp.com/terraform/0.12.13/terraform_0.12.13_linux_amd64.zip
+unzip terraform_0.12.13_linux_amd64.zip
+chmod +x terraform
+sudo mv terraform /usr/local/bin/
+rm terraform_0.12.13_linux_amd64.zip
+echo 'terraform Done.'
+
+sudo apt-get install -y jq mysql-client
+
