7145c12c0ee0af604db02fb148ea2a6883bf5c5b
Alan Chen <cchck91@gmail.com>
2020-03-20 21:31:33 +0800

    lock down os variant in download-bianry.sh

--- setup/download-bianry.sh
+++ setup/download-bianry.sh
@@ -7,10 +7,10 @@ echo "Get binaries ..."
 curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 echo 'helm Done.'
 
-curl --silent -LO "https://storage.googleapis.com/kubernetes-release/release/v1.17.4/bin/$(uname -s)/amd64/kubectl" && chmod +x kubectl && mv kubectl ~/bin/
+curl --silent -LO "https://storage.googleapis.com/kubernetes-release/release/v1.17.4/bin/linux/amd64/kubectl" && chmod +x kubectl && mv kubectl ~/bin/
 echo 'kubectl Done.'
 
-curl --silent -Lo aws-iam-authenticator "https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/$(uname -s)/amd64/aws-iam-authenticator" && chmod +x aws-iam-authenticator
+curl --silent -Lo aws-iam-authenticator "https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/linux/amd64/aws-iam-authenticator" && chmod +x aws-iam-authenticator
 sudo mv aws-iam-authenticator /usr/bin
 echo 'aws-iam-authenticator Done.'
 
