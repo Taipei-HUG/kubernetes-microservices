38627e67e69044582dbb4ad60a3049e606bdd38c
Alan Chen <cchck91@gmail.com>
2020-03-20 21:27:11 +0800

    fix download-binary.sh

--- setup/download-bianry.sh
+++ setup/download-bianry.sh
@@ -2,17 +2,15 @@
 
 mkdir ~/bin
 
-HOST_OS=$(uname -s)
-
 echo "Get binaries ..."
 
 curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 echo 'helm Done.'
 
-curl --silent -Lo kubectl curl -LO https://storage.googleapis.com/kubernetes-release/release/1.17.4/bin/$HOST_OS/amd64/kubectl && chmod +x kubectl && mv kubectl ~/bin/
+curl --silent -LO "https://storage.googleapis.com/kubernetes-release/release/v1.17.4/bin/$(uname -s)/amd64/kubectl" && chmod +x kubectl && mv kubectl ~/bin/
 echo 'kubectl Done.'
 
-curl --silent -Lo aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/$HOST_OS/amd64/aws-iam-authenticator && chmod +x aws-iam-authenticator
+curl --silent -Lo aws-iam-authenticator "https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/$(uname 0s)/amd64/aws-iam-authenticator" && chmod +x aws-iam-authenticator
 sudo mv aws-iam-authenticator /usr/bin
 echo 'aws-iam-authenticator Done.'
 
